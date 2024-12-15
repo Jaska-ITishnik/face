150243d6930c1d1dd8ce419de0668fb70c2638d4
Jaska-ITishnik <jasurbekbekmirzayev2004@gmail.com>
2024-12-15 11:33:29 +0500

    first commit

---
+++ test.py
@@ -0,0 +1,12 @@
+l = []
+nums = [1,1,0,1,1,1]
+i = 0
+while nums:
+    pass
+
+
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Jaska-ITishnik/face.git
+git push -u origin main
